b62f5901f2f129fe4ddd497f7a84ae56e1c71efc
Sam Brown <sam@sgb.io>
2020-06-19 20:27:08 +0100

    wip

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,47 @@ type FileWithContent = Octokit.ReposGetContentsResponse & {
   content: string
 }
 
+const SUPPORTED_EXTENSIONS = ['js', 'jsx', 'ts', 'tsx']
+
+const round = (num: number): number => Math.round(num * 100) / 100
+
+const getScoreEmoji = (score: number): string => {
+  if (score > 60) return ':warning:'
+  // if (score > 50) return ':warning:'
+  return ':white_check_mark:'
+}
+
+const formatComplexityScore = (score: number): string => {
+
+  const rounded = round(score)
+  const emoji = getScoreEmoji(score)
+
+  return `${rounded} ${emoji}`
+}
+
+const generateTableLines = (filteredResults: Array<Result>): string => {
+  return filteredResults.map((result) => {
+    const { filename, metrics } = result
+    const { lineEnd, dependencies, codehawkScore } = metrics
+    // TODO need previous score
+    return (
+      `| ${filename} | ${lineEnd} | ${dependencies.length} | 0 | ${formatComplexityScore(codehawkScore)} |`
+    )
+  }).join('\n')
+}
+
 const generatePrComment = (results: Array<Result>): string => {
+  const filteredResults = results.filter(r => !!r.metrics)
 
   return `
-    ### Codehawk Static Analysis Results
-    ${results.map((result) => {
-      return `
-        - ${result.filename}, ${JSON.stringify(result.metrics)}
-      `
-    })}
+  ## Codehawk Static Analysis Results
+
+  ${filteredResults.length} file${filteredResults.length > 1 ? 's' : ''} changed
+
+  | File | Total Lines | No. Dependencies | Complexity (before) | Complexity (after) |
+  | ---- | ----------- | ---------------- | ------------------- | ------------------ |
+  ${generateTableLines(filteredResults)}
+
   `
 }
 
@@ -42,11 +74,22 @@ const analyzeFiles = (
 
     const fileWithContents = content.data as FileWithContent
     const text = Buffer.from(fileWithContents.content, 'base64').toString()
-    console.log(text)
+    const { filename } = file
+
+    // extension
+    const extension = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2)
+    const isTypescript = extension === 'ts' || extension === 'tsx'
+
+    if (SUPPORTED_EXTENSIONS.indexOf(extension) < 0) {
+      return {
+        filename,
+        metrics: null
+      }
+    }
 
     return {
       filename: file.filename,
-      metrics: calculateComplexity(text)
+      metrics: calculateComplexity(text, extension, isTypescript, false) // Flow not supported
     }
   }))
 }
@@ -54,16 +97,23 @@ const analyzeFiles = (
 export = (app: Application) => {
 
   app.on('pull_request.edited', async (context) => {
-    
-
+    console.log('webhook received for pull_request.edited')
     const compare = await context.github.repos.compareCommits(context.repo({
       base: context.payload.pull_request.base.sha,
       head: context.payload.pull_request.head.sha
     }))
 
-    console.log(compare.data)
-    const analyzedFiles: any = await analyzeFiles(compare, context)
-    const comment = generatePrComment(analyzedFiles)
+    console.log('analyzing files...')
+    const analyzedFiles: Array<any> = await analyzeFiles(compare, context)
+    const filesWithMetrics = analyzedFiles.filter(f => !!f.metrics)
+
+    if (filesWithMetrics.length === 0) {
+      // Do nothing - no metrics were generated for this change
+      console.warn('no changes.')
+      return
+    }
+
+    const comment = generatePrComment(filesWithMetrics)
     const params = context.issue({ body: comment })
 
     await context.github.issues.createComment(params)
